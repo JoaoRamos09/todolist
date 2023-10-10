0a28a15d0d32721ae99cb03ce238100942815f72
Joao Ramos <joaorsramos@hotmail.com>
2023-10-10 16:30:30 -0300

    studying start

---
+++ todolist/src/test/java/br/com/joaoramos/todolist/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaoramos.todolist.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
